68c4abbfa910813eed326fb55c200d4fd611056a
李岚清 <958614130@qq.com>
2022-05-09 08:52:50 +0800

    ts demo

--- global.d.ts
+++ global.d.ts
@@ -1,11 +1,3 @@
-/*
- * @Author: your name
- * @Date: 2021-08-12 17:52:42
- * @LastEditTime: 2021-08-12 17:54:26
- * @LastEditors: Please set LastEditors
- * @Description: In User Settings Edit
- * @FilePath: \vue3.0-cli-ts\global.d.ts
- */
 interface Debug {
   mainPath: (string) => any;
 }
